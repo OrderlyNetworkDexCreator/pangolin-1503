c0fd8e6070cad66e44a2665354f7ebd2422cc9fd
Mario Reder <mario.reder@pm.me>
2025-12-01 00:36:20 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets,Leaderboard",
-  "VITE_CUSTOM_MENUS": "",
+  "VITE_CUSTOM_MENUS": "Swap, https://app.pangolin.exchange/swap; Pool, https://app.pangolin.exchange/pool; Stake, https://app.pangolin.exchange/stakexmas; Stats, https://app.pangolin.exchange/stats",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://t.me/PangolinV3",
